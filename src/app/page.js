8d2180564c26e95474c17cc36bcf9df618ad7e85
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-04-10 06:41:08 -0300

    ajust main page

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
           Paz Seja Convosco
         </h1>
         <div className='flex flex-row gap-10 mt-20'>
-          <Link className="bg-linear-to-b from-gradientButtonStart/100 from-0% to-gradientButtonEnd/100 to-100% py-3 px-10 text-button rounded-xl" href="">Livros</Link>
-          <Link className="bg-linear-to-b from-gradientButtonStart/100 from-0% to-gradientButtonEnd/100 to-100% py-3 px-10 text-button rounded-xl" href="">Sobre</Link>
+          <Link className="bg-linear-to-b from-gradientButtonStart/100 from-0% to-gradientButtonEnd/100 to-100% py-3 px-10 text-button rounded-xl hover:opacity-80 active:opacity-60" href="">Livros</Link>
+          <Link className="bg-linear-to-b from-gradientButtonStart/100 from-0% to-gradientButtonEnd/100 to-100% py-3 px-10 text-button rounded-xl hover:opacity-80 active:opacity-60" href="">Sobre</Link>
         </ div>
       </div>
       <span className='col-span-1' />
